cbf10aef0aced89f24e77ee0f1b193610e8ba8ad
iiky <ahmad.rizki.s@gmail.com>
2022-09-07 05:59:07 +0700

    Installed Cuba Template (Bootstrap 5)

---
+++ public/assets/js/datepicker/date-picker/datepicker.en.js
@@ -0,0 +1,29 @@
+"use strict";
+; (function ($) {
+    $.fn.datepicker.language['en'] = {
+        days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        daysShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        daysMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        today: 'Today',
+        clear: 'Clear',
+        dateFormat: 'mm/dd/yyyy',
+        timeFormat: 'hh:ii aa',
+        firstDay: 0
+    };
+
+    $.fn.datepicker.language['id'] = {
+        days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+        daysShort: ['M', 'S', 'S', 'R', 'K', 'J', 'S'],
+        daysMin: ['M', 'S', 'S', 'R', 'K', 'J', 'S'],
+        months: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+        today: 'Today',
+        clear: 'Clear',
+        dateFormat: 'dd-mm-yyyy',
+        timeFormat: 'hh:ii aa',
+        firstDay: 0
+    };
+
+})(jQuery);
